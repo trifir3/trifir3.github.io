bbdaf5f2ff5da9ec0dfa5ddc43fa2ee23556c8b9
trifir3 <trifir3@outlook.com>
2025-01-02 17:08:04 +0800

    change some styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'selector', // Enable selector strategy for dark mode
-	theme: {
-		fontFamily: {
-			'sans': ['Hack'],
-		},
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
-
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "selector", // Enable selector strategy for dark mode
+  theme: {
+    fontFamily: {
+      sans: ["Hack"],
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              background: "var(--tw-prose-hr)",
+              padding: ".125rem .25rem",
+              borderRadius: ".25rem",
+            },
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
