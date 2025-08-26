a58cba8001254f717168dc380332a9746f693adf
trifir3 <trifir3@outlook.com>
2025-08-26 11:56:15 +0800

    refactor blog layout and components; update styles and remove unused code

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss"
 
 export async function GET(context) {
   return rss({
-    title: "noblog",
-    description: "bloger cipher",
+    title: "trifir3's blog",
+    description: "A blog about web development",
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob("./posts/*.{md,mdx}")),
     customData: `<language>en-us</language>`,
